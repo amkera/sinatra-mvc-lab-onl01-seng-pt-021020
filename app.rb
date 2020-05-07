2723466d70a45b661c215dd2d897d07f8ed9778c
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-07 18:48:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @putinay_input = PigLatinizer.new.to_pig_latin(params[:user_phrase])
+    @putinay_input = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :piglatinized
   end
 end
